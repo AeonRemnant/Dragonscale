602385d6eb00b7ffeaf035670a7f5f586d6d8850
Sky Robinson <mr.skyrobinson@gmail.com>
2024-10-03 21:27:49 +1000

    Added docker container checking

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	containertypes "github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	defer cli.Close()
+
+	containers, err := cli.ContainerList(ctx, containertypes.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		fmt.Println(container.ID)
+	}
 }
